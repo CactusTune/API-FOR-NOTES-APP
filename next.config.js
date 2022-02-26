8452b305ca9df454b54563c795d3fb390ec1568c
CactusTune <ojoshegun42@gmail.com>
2022-02-26 00:19:55 -0800

    initial commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    env : {
+      MONGO_URI:"mongodb+srv://Shegun:AdebimpeOjo1@cluster0.kmjqr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    }
+}
---
+++ pages/api/test.js
@@ -0,0 +1,7 @@
+import dbConnect from "../../utils/dbconnect";
+
+dbConnect();
+
+export default async(req,res)=>{
+    res.json({test: 'test'})
+}
